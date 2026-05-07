124139bfb03adcde67e6bf4d222c9f4747e6cf3f
Julian Labbe <jlabbe@calpoly.edu>
2026-05-06 18:09:31 -0700

    feat: Added initial code and err library, as well as updated the rcopy and server to take in an error-rate runtime parameter

---
+++ server.c
@@ -0,0 +1,86 @@
+/* Server side - UDP Code				    */
+/* By Hugh Smith	4/1/2017	*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+
+#include "gethostbyname.h"
+#include "networks.h"
+#include "safeUtil.h"
+
+#define MAXBUF 80
+
+void processClient(int socketNum);
+int checkArgs(int argc, char *argv[]);
+
+int main ( int argc, char *argv[]  )
+{ 
+	int socketNum = 0;			
+	int portNumber = 0;
+	float errorRate = 0;	
+
+	// checkArgs returns the port number to use (if provided)
+	portNumber = checkArgs(argc, argv);
+	errorRate = atof(argv[1]);
+
+	printf("Error rate: %f\n", errorRate);
+		
+	socketNum = udpServerSetup(portNumber);
+
+	processClient(socketNum);
+
+	close(socketNum);
+	
+	return 0;
+}
+
+void processClient(int socketNum)
+{
+	int dataLen = 0; 
+	char buffer[MAXBUF + 1];	  
+	struct sockaddr_in6 client;		
+	int clientAddrLen = sizeof(client);	
+	
+	buffer[0] = '\0';
+	while (buffer[0] != '.')
+	{
+		dataLen = safeRecvfrom(socketNum, buffer, MAXBUF, 0, (struct sockaddr *) &client, &clientAddrLen);
+	
+		printf("Received message from client with ");
+		printIPInfo(&client);
+		printf(" Len: %d \'%s\'\n", dataLen, buffer);
+
+		// just for fun send back to client number of bytes received
+		sprintf(buffer, "bytes: %d", dataLen);
+		safeSendto(socketNum, buffer, strlen(buffer)+1, 0, (struct sockaddr *) & client, clientAddrLen);
+
+	}
+}
+
+int checkArgs(int argc, char *argv[])
+{
+	// Checks args and returns port number
+	int portNumber = 0;
+	
+	if (argc < 2 || argc > 4)
+	{
+		fprintf(stderr, "Usage %s error-rate [optional port number]\n", argv[0]);
+		exit(-1);
+	}
+	
+	if (argc == 3)
+	{
+		// Port number will be the third parameter
+		portNumber = atoi(argv[2]);
+	}
+	
+	return portNumber;
+}
+
+
